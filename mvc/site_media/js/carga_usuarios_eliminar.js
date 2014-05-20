5b077b8b7edda5c017f4324bc4847c24478b3ab1
Esther <arikhel@gmail.com>
2014-05-20 17:13:59 +0200

    creo funciones para cargar usuarios dinámicamente, función que saca un aviso por pantalla para confirmar la eliminación. Arreglo tabla de salida de usuarios

--- mvc/site_media/js/carga_usuarios_eliminar.js
+++ mvc/site_media/js/carga_usuarios_eliminar.js
@@ -2,19 +2,19 @@ function carga_resultados(){
 
 var id_usuario = $("input[name='id_usuario']").prop("value");
 var nombre = $("input[name='nombre']").prop("value");
-var papellido = $("input[name='papellido']").prop("value");
-var sapellido = $("input[name='sapellido']").prop("value");
-var nick = $("input[name='nick']").prop("value");
+var primer_apellido = $("input[name='primer_apellido']").prop("value");
+var segundo_apellido = $("input[name='segundo_apellido']").prop("value");
+var nick_usuario = $("input[name='nick_usuario']").prop("value");
 
 
 	 var request = $.ajax({
 	 url: "../php/eliminar_usuario.php",
 	 type: "POST",
-	 data: 'id_usuario='+ id_usuario +'&nombre='+ nombre+'&primer_apellido='+ papellido+'&seguno_apellido='+ sapellido+"'&nick_usuario'"+nick,
+	 data: 'nombre='+nombre+'&primer_apellido='+primer_apellido+'&segundo_apellido='+segundo_apellido+'&nick_usuario='+nick_usuario+'&id_usuario='+id_usuario,
 	 dataType: "html"
 	 });
 	 request.done(function( msg ) {
-	 $( "#resultado_consulta" ).html( msg );
+	 $( "#usuario_eliminar" ).html( msg );
 	 });
 	 request.fail(function( jqXHR, textStatus ) {
 	 alert( "Request failed: " + textStatus );
@@ -22,15 +22,21 @@ var nick = $("input[name='nick']").prop("value");
 }
 
 $( document ).ready(function() {
-
-	$("#usuario_eliminar").keyup(function(){
-	alert(11);
+ //alert(4444);
+	$("input").keyup(function(){
+	
  carga_resultados();
 
 	});
-	
-		/*$("select").change(function(){
- carga_resultados();
-
+	$("#eliminar").click(function(){
+	$("#confirmacion").append("<div class='notice marker-on-bottom'>Los usuarios marcados se eliminarán");
+	$("#confirmacion").append("<input type='submit'/>");
+	$("#confirmacion").append("</div>");
+});
+/*function pop_aviso(){
+$( document ).ready(function() {
+	$("#eliminar").click(function(){
+	$("#confirmacion").append("<div class='notice marker-on-bottom'>Los usuarios marcados se eliminarán");
+	$("#confirmacion").append("<input ype='button'/>");
 	});*/
 });
--- mvc/site_media/php/eliminar_usuario.php
+++ mvc/site_media/php/eliminar_usuario.php
@@ -3,32 +3,41 @@ include('funciones_icanyo.php');
 
 	if (isset($_POST['id_usuario'])){
 		$cerca1=($_POST['id_usuario']);
-		}else{
+	}else{
 		$cerca1=0;
 	}
+	
 	if (isset($_POST['nombre'])){
 		$cerca2=($_POST['nombre']);
-		if ($cerca2==''){$cerca2='';}
-	
-	}	
-	if (isset($_POST['papellido'])){
-		$cerca3=($_POST['papellido']);
-		if ($cerca3==''){$cerca3='';}
-	
-	}	
-		if (isset($_POST['sapellido'])){
-		$cerca4=($_POST['sapellido']);
-		if ($cerca4==''){$cerca4='';}
-	
+	}else{
+		$cerca2='';
 	}
-		if (isset($_POST['nick'])){
-		$cerca5=($_POST['nick']);
-		if ($cerca5==''){$cerca5='';}
 	
+	
+	if (isset($_POST['primer_apellido'])){
+		$cerca3=($_POST['primer_apellido']);
+	}else{
+		$cerca3='';
 	}
+	
+		
+	if (isset($_POST['segundo_apellido'])){
+		$cerca4=($_POST['segundo_apellido']);
+	}else{
+		$cerca4='';
+	}
+	
+	if (isset($_POST['nick_usuario'])){
+		$cerca5=($_POST['nick_usuario']);
+	}else{
+		$cerca5='';
+	}
+	
+
+	
 $conex= conectar_bdd();
 
-$query="select * from usuario where id_usuario between '%$cerca1%' and 3000 and nombre like '%$cerca2%' and  primer_apellido like '%$cerca3%' and segundo_apellido LIKE '%$cerca4%' and nick_usuario like '%$cerca5%' order by primer_apellido asc";
+$query="select * from usuario where id_usuario like '%$cerca1%' and nombre like '%$cerca2%' and  primer_apellido like '%$cerca3%' and segundo_apellido like '%$cerca4%' and nick_usuario like '%$cerca5%' order by primer_apellido asc";
 //echo $query;
 $consulta=mysql_query($query,$conex);
 
@@ -36,31 +45,30 @@ $total_consulta=mysql_num_rows($consulta);
 
 
 if($total_consulta > 0){
-echo "<table>";
+
+echo "<table class='table hovered'>";
 	echo "<tr>";
-	echo "Resultados";
-	echo "</tr>";
+		echo "<th>";
+		echo "Nombre ";
+		echo "</th>";
+			echo "<th>";
+			echo "Primer apellido ";
+			echo "</th>";
+			echo "<th>";
+			echo "Segundo apellido";
+			echo "</th>";
+			echo "<th>";
+			echo "Marcar";
+			echo "</th>";
+		echo "</tr>";
 	while ($fila = mysql_fetch_array($consulta)) {
 		echo "<tr>";
-		echo "<td><p>Nombre</p></td>";
-		
-		echo "<td>".$fila[1]."</td>";
-		echo "</tr>";
-		echo "<tr>";
-		echo "<td><p>Descripción</p></td>";
-	
-		echo "<td>".$fila[2]."</td>";
-		echo "</tr>";
-		echo "<tr>";
-		echo "<td><p>Intensidad</p></td>";
-	
-		echo "<td>".$fila[3]."</td>";
-		echo "</tr>";
-		echo "<tr>";
-		echo "<td><p>Precio</p></td>";
-		
-		echo "<td>".$fila[4]."</td>";
-		echo "</tr>";
+			echo "<td>".$fila[1]."</td>";
+			echo "<td>".$fila[2]."</td>";
+			echo "<td>".$fila[3]."</td>";
+			echo "<td><input type='checkbox'/></td>";
+			echo "</tr>";
+
 	}
 
 echo "</table>";
